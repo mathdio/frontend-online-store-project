92f3716f9eeb785e0ce10159085b6b3896450708
matheus <mathdiogenes@gmail.com>
2022-09-08 16:06:14 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,21 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const request = await fetch(url);
+  const response = await request.json();
+  return response;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (query === undefined) {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+    const request = await fetch(url);
+    const response = await request.json();
+    return response;
+  }
+  const url = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const request = await fetch(url);
+  const response = await request.json();
+  return response;
 }
 
 export async function getProductById() {
