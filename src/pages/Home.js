6fdfb827d1ecc893639a4efb3f07947aca88f336
matheus <mathdiogenes@gmail.com>
2022-09-08 17:50:18 -0300

    Requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   state = { search: '' };
@@ -24,6 +25,7 @@ class Home extends React.Component {
             Digite algum termo de pesquisa ou escolha uma categoria.
           </div>
         </label>
+        <Link data-testid="shopping-cart-button" to="/shoppingcart" />
       </div>
     );
   }
