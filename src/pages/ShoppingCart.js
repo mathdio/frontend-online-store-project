39d06e36c7049b8ec59788a7c307f2e653addc69
Lucas G <lucasg.devmail93@gmail.com>
2022-09-10 15:27:17 -0300

    requisito funciona mas não passa no teste

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,9 +1,58 @@
 import React from 'react';
 
 class ShoppingCart extends React.Component {
+  state = {
+    shopCartProducts: [],
+  };
+
+  componentDidMount() {
+    this.initShopCart();
+  }
+
+  initShopCart = () => {
+    const cartStorage = localStorage.getItem('shoppingCart');
+    const temp = cartStorage ? JSON.parse(cartStorage) : [];
+    this.setState({ shopCartProducts: temp });
+  };
+
+  noRepeatElementsArray = (array = []) => {
+    const arrayUnique = [];
+    const str = (obj) => JSON.stringify(obj);
+    array.forEach((e) => {
+      const temp = arrayUnique.some((i) => str(i) === str(e));
+      if (!temp) {
+        arrayUnique.push(e);
+      }
+    });
+    return arrayUnique;
+  };
+
+  renderCartComponents = () => {
+    const { shopCartProducts } = this.state;
+    const listFilter = this.noRepeatElementsArray(shopCartProducts);
+    return (
+      <div>
+        {(listFilter.length > 0) ? listFilter.map((e) => (
+
+          <div key={ e.id } data-testid="shopping-cart-product-name">
+            <img src={ e.thumbnail } alt={ e.title } />
+            <h3 data-testid="shopping-cart-product-name">{e.title}</h3>
+            <h3 data-testid="shopping-cart-product-quantity">
+              {`Qtd: ${shopCartProducts.filter((i) => (i.id === e.id)).length}`}
+            </h3>
+            <p>{e.price}</p>
+          </div>
+
+        )) : <h3>Seu carrinho está vazio</h3>}
+      </div>
+    );
+  };
+
   render() {
     return (
-      <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+      <span data-testid="shopping-cart-empty-message">
+        { this.renderCartComponents() ?? <h3>Seu carrinho está vazio</h3> }
+      </span>
     );
   }
 }
